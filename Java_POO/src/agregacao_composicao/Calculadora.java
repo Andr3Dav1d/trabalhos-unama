0742810a7f1916b02565529e2e8e0bfabd43da7f
André David <160057713+Andr3Dav1d@users.noreply.github.com>
2025-10-12 21:13:19 -0300

    Create Calculadora class with inputPane method

---
+++ Java_POO/src/agregacao_composicao/Calculadora.java
@@ -0,0 +1,12 @@
+package agregacao_composicao;
+import javax.swing.JOptionPane;
+
+public class Calculadora {
+
+    public static int inputPane(String pergunta) {
+        
+    }
+    public static void main(String[] args) {
+        
+    }
+}
